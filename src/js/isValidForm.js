592a7077c9223ef6061148a66c079807f11775b5
Samira Esmail Pur <samiraesmailpur05@gmail.com>
2023-07-28 15:22:29 +0300

    added form validation

--- src/js/isValidForm.js
+++ src/js/isValidForm.js
@@ -1,35 +1,70 @@
+const name = document.querySelector('.name');
+const surname = document.querySelector('.surname');
+const email = document.querySelector('.email');
 const form = document.querySelector('.subscribe__form');
+const errorName = document.querySelector('#error-name');
+const errorSurname = document.querySelector('#error-surname');
+const errorEmail = document.querySelector('#error-email');
 
-form.addEventListener('submit', handleSubmit);
+const date = new Date().toLocaleDateString('uk-UA');
 
-function handleSubmit(event) {
-  event.preventDefault();
+const emailRegex =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const fullNameRegex = /^[A-Z][a-zA-Z]*$/;
 
-  const name = form.elements.name.value;
-  const surname = form.elements.surname.value;
-  const email = form.elements.email.value;
-  const emailRegex =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+let messagesFullName =
+  'Only Latin letters are allowed. The first letter must be capitalized. Numbers and spaces are not allowed.';
+let messagesEmail = 'Please enter a valid email address';
 
-  const formData = {
-    name,
-    surname,
-    email,
-  };
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  let isValidForm = true;
 
-  if (formData.name.toLowerCase() === 'sigma') {
-    alert('wow');
+  if (name.value === '') {
+    errorName.textContent = messagesFullName;
+    isValidForm = false;
+  } else if (!fullNameRegex.test(name.value)) {
+    errorName.textContent = messagesFullName;
+    isValidForm = false;
+  } else {
+    errorName.textContent = '';
   }
 
-  // if (formData.name === '' || formData.surname === '' || formData.email === '') {
-  //   alert('Please fill in all fields');
-  // }
+  if (surname.value === '') {
+    errorSurname.textContent = messagesFullName;
+    isValidForm = false;
+  } else if (!fullNameRegex.test(surname.value)) {
+    errorSurname.textContent = messagesFullName;
+    isValidForm = false;
+  } else {
+    errorSurname.textContent = '';
+  }
 
-  // if (emailRegex.test(email)) {
-  //   console.log('good');
-  // } else {
-  //   console.log('bad');
-  // }
+  if (email.value === '') {
+    errorEmail.textContent = messagesEmail;
+    isValidForm = false;
+  } else if (!emailRegex.test(email.value)) {
+    errorEmail.textContent = messagesEmail;
+    isValidForm = false;
+  } else {
+    errorEmail.textContent = '';
+  }
 
-  // console.log(formData);
+  if (isValidForm) {
+    const formData = {
+      name: name.value,
+      surname: surname.value,
+      email: email.value,
+    };
+
+    localStorage.setItem('formData', JSON.stringify(formData));
+    form.reset();
+    getDiscount();
+  }
+});
+
+function getDiscount() {
+  const data = JSON.parse(localStorage.getItem('formData'));
+  if (data.name === 'Sigma') {
+  }
 }
